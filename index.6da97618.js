7ac590885aab8e1805f012aaf3ed0fb01fe4a3df
Victor Komara <vitioklss@gmail.com>
2024-08-08 14:41:21 +0300

    make build

---
+++ index.6da97618.js
@@ -0,0 +1,2 @@
+for(var e=document.querySelector("table").rows,l=0;l<e.length;l++){var o=e[l].cells[1],r=e[l].cells[4],c=o.cloneNode(!0),n=r.cloneNode(!0);r.remove(),e[l].append(c),e[l].append(n)}
+//# sourceMappingURL=index.6da97618.js.map
